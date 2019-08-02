ee975e514106945bb71e4fb506f417b925a45ea7
Engineering Tech <51522195+firebolt-minion@users.noreply.github.com>
2019-08-01 19:55:44 -0700

    Update cooler.ino
    cleanup

--- cooler.ino
+++ cooler.ino
@@ -14,26 +14,18 @@ DallasTemperature sensors(&oneWire);
 
 float Celsius = 0;
 float Fahrenheit = 0;
-
-/*
-int x = 1;
-int ButtonState = 0;
-int OldButtonState = LOW;
-int NewButtonState = digitalRead(Switch_pin);
-*/
-
 int Switch_pin = 5;
 int LED_pin = 22;
 int Relay_pin = 23;
-int val = 0;
-int val2 = 0;
+int switchVal = 0;
+int relayVal = 0;
 
+bool coolerOn = false;
 
 
 void setup() {
   sensors.begin();
   Serial.begin (115200);
-  
   pinMode(Switch_pin,INPUT_PULLDOWN);
   pinMode(Relay_pin,OUTPUT);
   digitalWrite(Relay_pin,LOW);
@@ -45,33 +37,33 @@ void loop() {
   Celsius = sensors.getTempCByIndex(0);
   Fahrenheit = sensors.toFahrenheit(Celsius);
 
-val = digitalRead(Switch_pin);
-val2 = digitalRead(Relay_pin);
+  switchVal = digitalRead(Switch_pin);
+  relayVal = digitalRead(Relay_pin);
 
-Serial.print("Switch State: ");
-Serial.print(val); 
-Serial.println();
-
-delay (1000);
-
-
-if (val == 1){
-digitalWrite(Relay_pin,HIGH);
-digitalWrite(LED_pin,HIGH);
- val2 = digitalRead(Relay_pin);
-Serial.print("Relay State: ");
-Serial.print(val2); 
-Serial.println();
-}
+  Serial.print("Switch State: ");
+  Serial.print(switchVal); 
+  Serial.println();
+  
+  delay (1000);
 
 
-else {
-  digitalWrite(Relay_pin,LOW);
-  digitalWrite(LED_pin,LOW);
- Serial.print("Relay State: ");
-Serial.print(val2); 
-Serial.println();
-}
+  if (switchVal == 1){
+    digitalWrite(Relay_pin,HIGH);
+    digitalWrite(LED_pin,HIGH);
+    relayVal = digitalRead(Relay_pin);
+    Serial.print("Relay State: ");
+    Serial.print(relayVal); 
+    Serial.println();
+  }
+  
+  
+  else {
+    digitalWrite(Relay_pin,LOW);
+    digitalWrite(LED_pin,LOW);
+    Serial.print("Relay State: ");
+    Serial.print(relayVal); 
+    Serial.println();
+  }
   Serial.print(Celsius);
   Serial.print(" C ");
   Serial.println();
